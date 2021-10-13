a56ca8da17987aa5a72b930ea621b56744b72b15
Kerr Rainey <kerr.rainey@digital.cabinet-office.gov.uk>
2021-10-13 11:49:17 +0100

    PYI-307: Create skeleton app
    A skeleton of the Orchestrator stub app. Displays a single page with GOV.UK branding.

---
+++ di-ipv-orchestrator-stub/src/main/java/uk/gov/di/ipv/stub/orc/App.java
@@ -0,0 +1,9 @@
+package uk.gov.di.ipv.stub.orc;
+
+public class App {
+
+    public static void main(String[] args) {
+        new Orchestrator();
+    }
+
+}
---
+++ di-ipv-orchestrator-stub/src/main/java/uk/gov/di/ipv/stub/orc/handlers/HomeHandler.java
@@ -0,0 +1,18 @@
+package uk.gov.di.ipv.stub.orc.handlers;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import uk.gov.di.ipv.stub.orc.utils.ViewHelper;
+
+import java.util.HashMap;
+
+public class HomeHandler {
+    public static Route serveHomePage = (Request request, Response response) -> {
+
+        var modelMap = new HashMap<String, Object>();
+        modelMap.put("welcome", "Hello world");
+
+        return ViewHelper.render(modelMap, "home.mustache");
+    };
+}
