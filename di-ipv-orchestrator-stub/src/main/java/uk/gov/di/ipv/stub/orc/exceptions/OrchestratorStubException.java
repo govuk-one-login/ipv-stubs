939829ea0a732b2096ebb0b78a683ddbd4b42e41
Danny Hayes <daniel.hayes@digital.cabinet-office.gov.uk>
2022-02-14 09:54:17 +0000

    PYI-691: Create a signed client auth JWT and pass to ipv-core within the /token request

---
+++ di-ipv-orchestrator-stub/src/main/java/uk/gov/di/ipv/stub/orc/exceptions/OrchestratorStubException.java
@@ -0,0 +1,7 @@
+package uk.gov.di.ipv.stub.orc.exceptions;
+
+public class OrchestratorStubException extends Exception {
+    public OrchestratorStubException(String errorMessage) {
+        super(errorMessage);
+    }
+}
--- di-ipv-orchestrator-stub/src/main/java/uk/gov/di/ipv/stub/orc/handlers/IpvHandler.java
+++ di-ipv-orchestrator-stub/src/main/java/uk/gov/di/ipv/stub/orc/handlers/IpvHandler.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jwt.SignedJWT;
 import com.nimbusds.oauth2.sdk.AuthorizationCode;
 import com.nimbusds.oauth2.sdk.AuthorizationCodeGrant;
 import com.nimbusds.oauth2.sdk.AuthorizationRequest;
@@ -15,6 +17,8 @@ import com.nimbusds.oauth2.sdk.SerializeException;
 import com.nimbusds.oauth2.sdk.TokenErrorResponse;
 import com.nimbusds.oauth2.sdk.TokenRequest;
 import com.nimbusds.oauth2.sdk.TokenResponse;
+import com.nimbusds.oauth2.sdk.auth.ClientAuthentication;
+import com.nimbusds.oauth2.sdk.auth.PrivateKeyJWT;
 import com.nimbusds.oauth2.sdk.http.HTTPRequest;
 import com.nimbusds.oauth2.sdk.http.HTTPResponse;
 import com.nimbusds.oauth2.sdk.id.ClientID;
@@ -31,10 +35,15 @@ import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import uk.gov.di.ipv.stub.orc.exceptions.OrchestratorStubException;
+import uk.gov.di.ipv.stub.orc.utils.JwtHelper;
 import uk.gov.di.ipv.stub.orc.utils.ViewHelper;
 
 import java.io.IOException;
 import java.net.URI;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -43,8 +52,8 @@ import java.util.Map;
 import static uk.gov.di.ipv.stub.orc.config.OrchestratorConfig.IPV_BACKCHANNEL_ENDPOINT;
 import static uk.gov.di.ipv.stub.orc.config.OrchestratorConfig.IPV_BACKCHANNEL_TOKEN_PATH;
 import static uk.gov.di.ipv.stub.orc.config.OrchestratorConfig.IPV_BACKCHANNEL_USER_IDENTITY_PATH;
-import static uk.gov.di.ipv.stub.orc.config.OrchestratorConfig.IPV_CLIENT_ID;
 import static uk.gov.di.ipv.stub.orc.config.OrchestratorConfig.IPV_ENDPOINT;
+import static uk.gov.di.ipv.stub.orc.config.OrchestratorConfig.ORCHESTRATOR_CLIENT_ID;
 import static uk.gov.di.ipv.stub.orc.config.OrchestratorConfig.ORCHESTRATOR_REDIRECT_URL;
 
 public class IpvHandler {
@@ -57,7 +66,7 @@ public class IpvHandler {
         stateSession.put(state.getValue(), null);
 
         var authRequest = new AuthorizationRequest.Builder(
-                new ResponseType(ResponseType.Value.CODE), new ClientID(IPV_CLIENT_ID))
+                new ResponseType(ResponseType.Value.CODE), new ClientID(ORCHESTRATOR_CLIENT_ID))
                 .state(state)
                 .scope(new Scope("openid"))
                 .redirectionURI(new URI(ORCHESTRATOR_REDIRECT_URL))
@@ -71,17 +80,22 @@ public class IpvHandler {
     public Route doCallback = (Request request, Response response) -> {
         var authorizationCode = getAuthorizationCode(request);
 
-        var accessToken = exchangeCodeForToken(authorizationCode);
-
-        var userInfo = getUserInfo(accessToken);
         List<Map<String, Object>> mustacheData = new ArrayList<>();
         Map<String, Object> moustacheDataModel = new HashMap<>();
 
         try {
-            mustacheData = buildMustacheData(userInfo);
-            moustacheDataModel.put("data", mustacheData);
-        } catch (ParseException | JsonSyntaxException e) {
-            moustacheDataModel.put("error", userInfo.toJSONString());
+            var accessToken = exchangeCodeForToken(authorizationCode);
+
+            var userInfo = getUserInfo(accessToken);
+
+            try {
+                mustacheData = buildMustacheData(userInfo);
+                moustacheDataModel.put("data", mustacheData);
+            } catch (ParseException | JsonSyntaxException e) {
+                moustacheDataModel.put("error", userInfo.toJSONString());
+            }
+        } catch (OrchestratorStubException e) {
+            moustacheDataModel.put("error", e.getMessage());
         }
 
         return ViewHelper.render(moustacheDataModel, "userinfo.mustache");
@@ -100,12 +114,23 @@ public class IpvHandler {
                 .getAuthorizationCode();
     }
 
-    private AccessToken exchangeCodeForToken(AuthorizationCode authorizationCode) {
+    private AccessToken exchangeCodeForToken(AuthorizationCode authorizationCode) throws OrchestratorStubException, CertificateException, JOSEException {
         URI resolve = URI.create(IPV_BACKCHANNEL_ENDPOINT).resolve(IPV_BACKCHANNEL_TOKEN_PATH);
         logger.info("token url is " + resolve);
+
+        SignedJWT signedClientJwt;
+        try {
+            signedClientJwt = JwtHelper.createSignedClientAuthJwt();
+        } catch (JOSEException | InvalidKeySpecException | NoSuchAlgorithmException e) {
+           logger.error("Failed to generate orch client JWT", e);
+           throw new OrchestratorStubException("Failed to generate orch client JWT");
+        }
+
+        ClientAuthentication clientAuthentication = new PrivateKeyJWT(signedClientJwt);
+
         TokenRequest tokenRequest = new TokenRequest(
                 resolve,
-                new ClientID(IPV_CLIENT_ID),
+                clientAuthentication,
                 new AuthorizationCodeGrant(authorizationCode, URI.create(ORCHESTRATOR_REDIRECT_URL))
         );
 
@@ -115,7 +140,7 @@ public class IpvHandler {
         if (tokenResponse instanceof TokenErrorResponse) {
             TokenErrorResponse errorResponse = tokenResponse.toErrorResponse();
             logger.error("Failed to get token: " + errorResponse.getErrorObject());
-            return null;
+            throw new OrchestratorStubException(errorResponse.getErrorObject().getDescription());
         }
 
         return tokenResponse
