3e6c3404372a37a4b04bfa502392cd287a7fffe9
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2022-05-11 13:08:57 +0100

    refactor: Mount config dir instead of copying into image

--- di-ipv-core-stub/startup.sh
+++ di-ipv-core-stub/startup.sh
@@ -3,16 +3,9 @@ set -eu
 
 CONFIG_DIR="../../di-ipv-config/stubs/di-ipv-core-stub"
 
-clean_up () {
-    rm -r config
-}
-trap clean_up EXIT
-
 if [ ! -d "$CONFIG_DIR" ]; then
   { echo "🛑 config dir '$CONFIG_DIR' does not exist in expected location"; exit 1; }
 fi
 
-cp -r $CONFIG_DIR config
-
 docker build -t ipv-core-stub .
-docker run -p 8085:8085 --env-file ${CONFIG_DIR}/.env ipv-core-stub
+docker run -p 8085:8085 --env-file ${CONFIG_DIR}/.env --mount type=bind,source="$(pwd)/$CONFIG_DIR",target=/app/config ipv-core-stub
