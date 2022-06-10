0c95c34827cb690a85007c0a5b21d9bbdce4be93
Jamie Bentham <jamie.bentham@digital.cabinet-office.gov.uk>
2022-06-10 10:24:33 +0100

    KBV-484: Minor patch to allow testing of fraud check with users who have house names

--- di-ipv-core-stub/src/main/java/uk/gov/di/ipv/stub/core/config/uatuser/UKAddress.java
+++ di-ipv-core-stub/src/main/java/uk/gov/di/ipv/stub/core/config/uatuser/UKAddress.java
@@ -1,8 +1,9 @@
 package uk.gov.di.ipv.stub.core.config.uatuser;
 
 public record UKAddress(
-        String street1,
-        String street2,
+        String buildingNumber,
+        String buildingName,
+        String street,
         String county,
         String townCity,
         String postCode,
