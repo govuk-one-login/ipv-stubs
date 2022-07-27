57c5bb5a636be44c46ef7cd9ca06e09ee93ef941
Jamie Bentham <jamie.bentham@digital.cabinet-office.gov.uk>
2022-07-27 18:27:40 +0100

    KBV-771: Updating the stub so valid from and valid until can be set and a second address can be added under new user KBV-771: Updating IDs to make automation easier KBV-771: Addressed tech QA comments

--- di-ipv-core-stub/src/main/java/uk/gov/di/ipv/stub/core/config/uatuser/Identity.java
+++ di-ipv-core-stub/src/main/java/uk/gov/di/ipv/stub/core/config/uatuser/Identity.java
@@ -1,10 +1,12 @@
 package uk.gov.di.ipv.stub.core.config.uatuser;
 
+import java.util.List;
+
 public record Identity(
         int rowNumber,
         String accountNumber,
         String ctdbDatabase,
-        UKAddress UKAddress,
+        List<UKAddress> addresses,
         FindDateOfBirth findDateOfBirth,
         FullName name,
         Questions questions) {}
