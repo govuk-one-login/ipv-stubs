9390ecffadab36591bc84c6042e8db9c88976086
vam <vam.hedayati@digital.cabinet-office.gov.uk>
2023-03-02 13:59:54 +0000

    PYIC-2506 making changes Patrick pointed out - hopefully this is correct

--- di-ipv-core-stub/deploy/cri/authorizer/index.js
+++ di-ipv-core-stub/deploy/cri/authorizer/index.js
@@ -1,6 +1,17 @@
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'eu-west-2'});
 
+async function getParam {
+    const ssm = new AWS.SSM({region: 'eu-west-2'});
+
+    const paramDetails = await ssm.getParameter({
+      Name: '/stubs/core/cri/env/CORE_STUB_API_AUTH', /* required */
+      WithDecryption: false
+    }).promise();
+    const data = JSON.parse(paramDetails.Parameter.Value);
+    return data;
+};
+
 export const handler = async(event) => {
     let response = {
         "isAuthorized": false,
@@ -10,11 +21,11 @@ export const handler = async(event) => {
     };
 
     const auth = await getParam();
-    var authorizationHeader = event.headers.Authorization
-    var encodedCreds = authorizationHeader.split(' ')[1]
-    var plainCreds = (new Buffer(encodedCreds, 'base64')).toString().split(':')
-    var username = plainCreds[0]
-    var password = plainCreds[1]
+    const authorizationHeader = event.headers.Authorization
+    const encodedCreds = authorizationHeader.split(' ')[1]
+    const plainCreds = (new Buffer(encodedCreds, 'base64')).toString().split(':')
+    const username = plainCreds[0]
+    const password = plainCreds[1]
 
     if (username === auth.username && password === auth.password) {
         response = {
@@ -27,13 +38,4 @@ export const handler = async(event) => {
     return response;
 };
 
-async function getParam {
-    const ssm = new AWS.SSM({region: 'eu-west-2'});
 
-    const paramDetails = await ssm.getParameter({
-      Name: '/stubs/core/cri/env/CORE_STUB_API_AUTH', /* required */
-      WithDecryption: false
-    }).promise();
-    const data = JSON.parse(paramDetails.Parameter.Value);
-    return data;
-};
