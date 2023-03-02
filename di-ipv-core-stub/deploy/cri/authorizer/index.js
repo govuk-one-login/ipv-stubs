cb2fd5a7ba35b65cf04f788a69636f7124e349ed
vam <vam.hedayati@digital.cabinet-office.gov.uk>
2023-03-02 13:46:22 +0000

    PYIC-2506 Core Cri Stub Added Lambda Auth
    Core Passport Stub - changed dns name to passport.core.domain
    
    	new file:   di-ipv-core-stub/deploy/cri/authorizer/index.js
    	modified:   di-ipv-core-stub/deploy/cri/template.yaml
    	modified:   di-ipv-core-stub/deploy/passport/template.yaml

---
+++ di-ipv-core-stub/deploy/cri/authorizer/index.js
@@ -0,0 +1,39 @@
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'eu-west-2'});
+
+export const handler = async(event) => {
+    let response = {
+        "isAuthorized": false,
+        "context": {
+            "Authorization": "failed"
+        }
+    };
+
+    const auth = await getParam();
+    var authorizationHeader = event.headers.Authorization
+    var encodedCreds = authorizationHeader.split(' ')[1]
+    var plainCreds = (new Buffer(encodedCreds, 'base64')).toString().split(':')
+    var username = plainCreds[0]
+    var password = plainCreds[1]
+
+    if (!(username === auth.username && password === auth.password)) {
+        response = {
+            "isAuthorized": true,
+            "context": {
+                "Authorization": "Succeeded"
+            }
+        };
+    }
+    return response;
+};
+
+async function getParam {
+    const ssm = new AWS.SSM({region: 'eu-west-2'});
+
+    const paramDetails = await ssm.getParameter({
+      Name: '/stubs/core/cri/env/CORE_STUB_API_AUTH', /* required */
+      WithDecryption: false
+    }).promise();
+    const data = JSON.parse(paramDetails.Parameter.Value);
+    return data;
+};
