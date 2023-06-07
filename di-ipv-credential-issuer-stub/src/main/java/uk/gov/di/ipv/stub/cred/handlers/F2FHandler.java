a5c3c9004a4fa062fc1393ee6c2b5923d194a6c8
Laurence Webb <laurence.webb@digital.cabinet-office.gov.uk>
2023-06-07 12:36:12 +0100

    BAU Handle subject value from vc in f2f handler

--- di-ipv-credential-issuer-stub/src/main/java/uk/gov/di/ipv/stub/cred/handlers/F2FHandler.java
+++ di-ipv-credential-issuer-stub/src/main/java/uk/gov/di/ipv/stub/cred/handlers/F2FHandler.java
@@ -1,5 +1,7 @@
 package uk.gov.di.ipv.stub.cred.handlers;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jwt.SignedJWT;
 import com.nimbusds.oauth2.sdk.OAuth2Error;
 import com.nimbusds.oauth2.sdk.ParseException;
 import com.nimbusds.oauth2.sdk.id.Subject;
@@ -9,22 +11,33 @@ import org.eclipse.jetty.http.HttpHeader;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import uk.gov.di.ipv.stub.cred.domain.Credential;
+import uk.gov.di.ipv.stub.cred.service.CredentialService;
 import uk.gov.di.ipv.stub.cred.service.TokenService;
 import uk.gov.di.ipv.stub.cred.validation.ValidationResult;
 import uk.gov.di.ipv.stub.cred.validation.Validator;
+import uk.gov.di.ipv.stub.cred.vc.VerifiableCredentialGenerator;
 
 import javax.servlet.http.HttpServletResponse;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Objects;
-import java.util.UUID;
 
 public class F2FHandler {
 
     private static final String JSON_RESPONSE_TYPE = "application/json;charset=UTF-8";
     private TokenService tokenService;
+    private CredentialService credentialService;
+    private VerifiableCredentialGenerator verifiableCredentialGenerator;
 
-    public F2FHandler(TokenService tokenService) {
+    public F2FHandler(
+            CredentialService credentialService,
+            TokenService tokenService,
+            VerifiableCredentialGenerator verifiableCredentialGenerator) {
+        this.credentialService = credentialService;
         this.tokenService = tokenService;
+        this.verifiableCredentialGenerator = verifiableCredentialGenerator;
     }
 
     public Route getResource =
@@ -37,13 +50,26 @@ public class F2FHandler {
                     return validationResult.getError().getDescription();
                 }
 
+                String resourceId = tokenService.getPayload(accessTokenString);
+                Credential credential = credentialService.getCredential(resourceId);
+
+                String verifiableCredential;
+                try {
+                    verifiableCredential =
+                            verifiableCredentialGenerator.generate(credential).serialize();
+                } catch (NoSuchAlgorithmException | InvalidKeySpecException | JOSEException e) {
+                    response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                    return String.format("Error: Unable to generate VC - '%s'", e.getMessage());
+                }
+
                 tokenService.revoke(accessTokenString);
 
                 response.type(JSON_RESPONSE_TYPE);
                 response.status(HttpServletResponse.SC_ACCEPTED);
 
-                var userInfo =
-                        new UserInfo(new Subject("urn:fdc:gov.uk:2022:" + UUID.randomUUID()));
+                String subject =
+                        SignedJWT.parse(verifiableCredential).getJWTClaimsSet().getSubject();
+                var userInfo = new UserInfo(new Subject(subject));
                 userInfo.setClaim("https://vocab.account.gov.uk/v1/credentialStatus", "pending");
 
                 return userInfo.toJSONString();
--- di-ipv-credential-issuer-stub/src/test/java/uk/gov/di/ipv/stub/cred/handlers/F2FHandlerTest.java
+++ di-ipv-credential-issuer-stub/src/test/java/uk/gov/di/ipv/stub/cred/handlers/F2FHandlerTest.java
@@ -1,5 +1,10 @@
 package uk.gov.di.ipv.stub.cred.handlers;
 
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.ECDSASigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import com.nimbusds.oauth2.sdk.token.AccessToken;
 import com.nimbusds.oauth2.sdk.token.BearerAccessToken;
 import com.nimbusds.oauth2.sdk.util.JSONObjectUtils;
@@ -12,18 +17,28 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import spark.Request;
 import spark.Response;
+import uk.gov.di.ipv.stub.cred.fixtures.TestFixtures;
+import uk.gov.di.ipv.stub.cred.service.CredentialService;
 import uk.gov.di.ipv.stub.cred.service.TokenService;
+import uk.gov.di.ipv.stub.cred.vc.VerifiableCredentialGenerator;
 
 import javax.servlet.http.HttpServletResponse;
 
-import java.util.UUID;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.ECPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
 
-import static org.hamcrest.CoreMatchers.startsWithIgnoringCase;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.gov.di.ipv.stub.cred.handlers.AuthorizeHandler.SHARED_CLAIMS;
 
 @ExtendWith(MockitoExtension.class)
 public class F2FHandlerTest {
@@ -32,13 +47,17 @@ public class F2FHandlerTest {
     @Mock private Response mockResponse;
     @Mock private Request mockRequest;
     @Mock private TokenService mockTokenService;
+    @Mock private CredentialService mockCredentialService;
+    @Mock private VerifiableCredentialGenerator mockVerifiableCredentialGenerator;
     private F2FHandler resourceHandler;
     private AccessToken accessToken;
 
     @BeforeEach
     void setup() {
         accessToken = new BearerAccessToken();
-        resourceHandler = new F2FHandler(mockTokenService);
+        resourceHandler =
+                new F2FHandler(
+                        mockCredentialService, mockTokenService, mockVerifiableCredentialGenerator);
     }
 
     @Test
@@ -46,6 +65,8 @@ public class F2FHandlerTest {
         when(mockTokenService.getPayload(accessToken.toAuthorizationHeader()))
                 .thenReturn(UUID.randomUUID().toString());
         when(mockRequest.headers("Authorization")).thenReturn(accessToken.toAuthorizationHeader());
+        when(mockVerifiableCredentialGenerator.generate(any()))
+                .thenReturn(signedRequestJwt(validRequestJWT()));
 
         JSONObject jsonResponse =
                 JSONObjectUtils.parse(
@@ -53,16 +74,14 @@ public class F2FHandlerTest {
 
         UserInfo docAppUserInfo = new UserInfo(jsonResponse);
 
-        assertThat(
-                docAppUserInfo.getSubject().getValue(),
-                startsWithIgnoringCase("urn:fdc:gov.uk:2022:"));
+        assertEquals("subject", docAppUserInfo.getSubject().getValue());
         assertEquals(
                 "pending",
                 docAppUserInfo.getClaim("https://vocab.account.gov.uk/v1/credentialStatus"));
 
         verify(mockResponse).type(JSON_RESPONSE_TYPE);
         verify(mockResponse).status(HttpServletResponse.SC_ACCEPTED);
-        verify(mockTokenService, times(1)).getPayload(accessToken.toAuthorizationHeader());
+        verify(mockTokenService, times(2)).getPayload(accessToken.toAuthorizationHeader());
         verify(mockTokenService).revoke(accessToken.toAuthorizationHeader());
     }
 
@@ -96,5 +115,51 @@ public class F2FHandlerTest {
 
         assertEquals("Client authentication failed", result);
         verify(mockResponse).status(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+
+    private JWTClaimsSet validRequestJWT() {
+        Instant instant = Instant.now();
+
+        Map<String, Object> sharedClaims = new LinkedHashMap<>();
+        sharedClaims.put("addresses", Collections.singletonList("123 random street, M13 7GE"));
+        sharedClaims.put(
+                "names",
+                List.of(
+                        Map.of(
+                                "nameParts",
+                                Arrays.asList(
+                                        Map.of("value", "Test"),
+                                        Map.of("value", "Testing"),
+                                        Map.of("value", "Tested")))));
+        sharedClaims.put("birthDate", List.of(Map.of("value", "01/01/1980")));
+
+        return new JWTClaimsSet.Builder()
+                .issuer("issuer")
+                .audience("audience")
+                .subject("subject")
+                .claim("redirect_uri", "https://valid.example.com")
+                .claim("response_type", "code")
+                .claim("state", "test-state")
+                .expirationTime(Date.from(instant.plus(1L, ChronoUnit.HOURS)))
+                .notBeforeTime(Date.from(instant))
+                .issueTime(Date.from(instant))
+                .claim(SHARED_CLAIMS, sharedClaims)
+                .build();
+    }
+
+    private ECPrivateKey getPrivateKey() throws InvalidKeySpecException, NoSuchAlgorithmException {
+        return (ECPrivateKey)
+                KeyFactory.getInstance("EC")
+                        .generatePrivate(
+                                new PKCS8EncodedKeySpec(
+                                        Base64.getDecoder().decode(TestFixtures.EC_PRIVATE_KEY_1)));
+    }
+
+    private SignedJWT signedRequestJwt(JWTClaimsSet claimsSet) throws Exception {
+        ECDSASigner ecdsaSigner = new ECDSASigner(getPrivateKey());
+        SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.ES256), claimsSet);
+        signedJWT.sign(ecdsaSigner);
+
+        return signedJWT;
     }
 }
