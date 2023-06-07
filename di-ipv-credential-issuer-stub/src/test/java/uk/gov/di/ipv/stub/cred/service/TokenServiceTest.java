3c555b98f96f2ea8e746dc21dc3537e002f46f4f
Laurence Webb <laurence.webb@digital.cabinet-office.gov.uk>
2023-06-07 14:32:06 +0100

    Fix imports

--- di-ipv-credential-issuer-stub/src/test/java/uk/gov/di/ipv/stub/cred/service/TokenServiceTest.java
+++ di-ipv-credential-issuer-stub/src/test/java/uk/gov/di/ipv/stub/cred/service/TokenServiceTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.gov.di.ipv.stub.cred.validation.ValidationResult;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
