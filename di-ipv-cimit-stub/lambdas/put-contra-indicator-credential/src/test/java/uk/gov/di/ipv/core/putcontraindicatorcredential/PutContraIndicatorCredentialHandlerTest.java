30ed1a336634e8fd0be3f3361190be4a96dd9771
“Talat <talat.aydin@digital.cabinet-office.gov.uk>
2023-07-14 22:02:10 +0100

    Create initial `putContraIndicators` stub lambda

---
+++ di-ipv-cimit-stub/lambdas/put-contra-indicator-credential/src/test/java/uk/gov/di/ipv/core/putcontraindicatorcredential/PutContraIndicatorCredentialHandlerTest.java
@@ -0,0 +1,31 @@
+package uk.gov.di.ipv.core.putcontraindicatorcredential;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.di.ipv.core.putcontraindicatorcredential.domain.PutContraIndicatorCredentialRequest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class PutContraIndicatorCredentialHandlerTest {
+
+    @Mock private Context mockContext;
+
+    @InjectMocks private PutContraIndicatorCredentialHandler classToTest;
+
+    @Test
+    void shouldReturnGetCiResponseWhenProvidedValidRequest() {
+        PutContraIndicatorCredentialRequest putContraIndicatorCredentialRequest =
+                PutContraIndicatorCredentialRequest.builder()
+                        .govukSigninJourneyId("govuk_signin_journey_id")
+                        .ipAddress("ip_address")
+                        .signedJwt("signed_jwts")
+                        .build();
+
+        assertEquals("Success", classToTest.handleRequest(putContraIndicatorCredentialRequest, mockContext));
+    }
+}
