650345fcb62ccc49d9310c3556e809f6f3eb70f9
“Talat <talat.aydin@digital.cabinet-office.gov.uk>
2023-07-17 14:54:19 +0100

    Create initial `putContraIndicators` stub lambda

---
+++ di-ipv-cimit-stub/lambdas/put-contra-indicator-credential/src/main/java/uk/gov/di/ipv/core/putcontraindicatorcredential/PutContraIndicatorCredentialHandler.java
@@ -0,0 +1,20 @@
+package uk.gov.di.ipv.core.putcontraindicatorcredential;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.di.ipv.core.putcontraindicatorcredential.domain.PutContraIndicatorCredentialRequest;
+
+public class PutContraIndicatorCredentialHandler
+        implements RequestHandler<PutContraIndicatorCredentialRequest, String> {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(PutContraIndicatorCredentialHandler.class);
+
+    @Override
+    public String handleRequest(PutContraIndicatorCredentialRequest event, Context context) {
+        LOGGER.info("EVENT TYPE: " + event.getClass().toString());
+        return "Success";
+    }
+}
