a8ca52b563b893c6d0bf597616ff908b57daab27
Rohit Payal <rohit.payal@digital.cabinet-office.gov.uk>
2023-07-19 12:28:06 +0100

    PYIC-3114 Added new cimit stub base proj. for cimit stub lambda's, added getContraIndicators lamdba with test.

---
+++ di-ipv-cimit-stub/lambdas/get-contra-indicators/src/test/java/uk/gov/di/ipv/core/getcontraindicators/GetContraIndicatorsHandlerTest.java
@@ -0,0 +1,31 @@
+package uk.gov.di.ipv.core.getcontraindicators;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.di.ipv.core.getcontraindicators.domain.GetCiRequest;
+import uk.gov.di.ipv.core.getcontraindicators.domain.GetCiResponse;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+class GetContraIndicatorsHandlerTest {
+    @Mock private Context mockContext;
+    @InjectMocks private GetContraIndicatorsHandler classToTest;
+
+    @Test
+    void shouldReturnGetCiResponseWhenProvidedValidRequest() {
+        GetCiRequest getCiRequest =
+                GetCiRequest.builder()
+                        .govukSigninJourneyId("govuk_signin_journey_id")
+                        .ipAddress("ip_address")
+                        .userId("user_id")
+                        .build();
+
+        GetCiResponse response = classToTest.handleRequest(getCiRequest, mockContext);
+        assertTrue(response.getContraIndicators().isEmpty());
+    }
+}
