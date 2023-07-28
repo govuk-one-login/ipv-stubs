0ab8b9528a102bd7186f0ea9016d7ba232f393b4
Rohit Payal <rohit.payal@digital.cabinet-office.gov.uk>
2023-07-28 17:19:47 +0100

    PYIC-3290 Added a test when no CI's in db for the user.

--- di-ipv-cimit-stub/lambdas/get-contra-indicators/src/test/java/uk/gov/di/ipv/core/getcontraindicators/GetContraIndicatorsHandlerTest.java
+++ di-ipv-cimit-stub/lambdas/get-contra-indicators/src/test/java/uk/gov/di/ipv/core/getcontraindicators/GetContraIndicatorsHandlerTest.java
@@ -19,6 +19,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -84,6 +85,28 @@ class GetContraIndicatorsHandlerTest {
 
         assertFalse(response.getContraIndicators().isEmpty());
         assertEquals(CI_V_03, response.getContraIndicators().get(0).getCi());
+    }
+
+    @Test
+    void shouldReturnGetCiResponseWhenProvidedValidRequestAndNoCiInDb() throws IOException {
+        GetCiRequest getCiRequest =
+                GetCiRequest.builder()
+                        .govukSigninJourneyId("govuk_signin_journey_id")
+                        .ipAddress("ip_address")
+                        .userId("user_id")
+                        .build();
+        List<CimitStubItem> cimitStubItems = new ArrayList<>();
+
+        when(mockCimitStubItemService.getCIsForUserId(USER_ID)).thenReturn(Collections.emptyList());
+
+        var response =
+                makeRequest(
+                        classToTest,
+                        mapper.writeValueAsString(getCiRequest),
+                        mockContext,
+                        GetCiResponse.class);
+
+        assertTrue(response.getContraIndicators().isEmpty());
     }
 
     @Test
