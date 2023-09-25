3914114dfae3709deaa598e5df612ae72c304492
Shivanshu Patel <shivanshu.patel@digital.cabinet-office.gov.uk>
2023-09-25 13:58:07 +0100

    PYIC-3318: generate and display uuid on start page

--- di-ipv-orchestrator-stub/src/main/java/uk/gov/di/ipv/stub/orc/handlers/HomeHandler.java
+++ di-ipv-orchestrator-stub/src/main/java/uk/gov/di/ipv/stub/orc/handlers/HomeHandler.java
@@ -5,11 +5,18 @@ import spark.Response;
 import spark.Route;
 import uk.gov.di.ipv.stub.orc.utils.ViewHelper;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class HomeHandler {
     public static final String APP_JOURNEY_USER_ID_PREFIX = "urn:uuid:app-journey-user-";
     public static final String NON_APP_JOURNEY_USER_ID_PREFIX = "urn:uuid:";
     public static Route serveHomePage =
             (Request request, Response response) -> {
-                return ViewHelper.render(null, "home.mustache");
+                Map<String, Object> moustacheDataModel = new HashMap<>();
+                String uuid = NON_APP_JOURNEY_USER_ID_PREFIX + UUID.randomUUID();
+                moustacheDataModel.put("uuid", uuid);
+                return ViewHelper.render(moustacheDataModel, "home.mustache");
             };
 }
