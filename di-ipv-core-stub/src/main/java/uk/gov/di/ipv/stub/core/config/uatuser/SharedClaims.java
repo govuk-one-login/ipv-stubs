cbd93bae81e2acaa6c9e9a0ebce83e1b99d7c14b
Jamie Bentham <jamie.bentham@digital.cabinet-office.gov.uk>
2024-06-05 12:00:19 +0100

    LIME-1121: Adding jackson annotations to ensure social security record is not sent if null

--- di-ipv-core-stub/src/main/java/uk/gov/di/ipv/stub/core/config/uatuser/SharedClaims.java
+++ di-ipv-core-stub/src/main/java/uk/gov/di/ipv/stub/core/config/uatuser/SharedClaims.java
@@ -1,9 +1,13 @@
 package uk.gov.di.ipv.stub.core.config.uatuser;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record SharedClaims(
         @JsonProperty("@context") List<String> context,
         @JsonProperty("name") List<Name> name,
