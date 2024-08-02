6aa57c31d549ce325323809f00e695b7f09de364
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2024-08-02 15:05:58 +0100

    PYIC-6083 Preserve EVCS metadata on update if not specified

--- di-ipv-evcs-stub/lambdas/src/common/apiResponses.ts
+++ di-ipv-evcs-stub/lambdas/src/common/apiResponses.ts
@@ -1,14 +1,12 @@
 import { APIGatewayProxyResultV2 } from "aws-lambda";
 
 export function buildApiResponse(
-  body: object,
+  body?: object,
   statusCode: number = 200,
 ): APIGatewayProxyResultV2 {
   return {
     statusCode,
-    body: JSON.stringify(body),
-    headers: {
-      "content-type": "application/json",
-    },
+    body: body && JSON.stringify(body),
+    headers: body ? { "content-type": "application/json" } : {},
   };
 }
