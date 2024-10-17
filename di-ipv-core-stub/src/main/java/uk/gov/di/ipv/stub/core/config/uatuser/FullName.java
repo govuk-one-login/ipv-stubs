abc3f928dc8c7c6249740097b58f45c1b93a65b8
Suraj Kumar <sk96.uk@gmail.com>
2024-10-17 15:59:42 +0100

    BAU: Spotless

--- di-ipv-core-stub/src/main/java/uk/gov/di/ipv/stub/core/config/uatuser/FullName.java
+++ di-ipv-core-stub/src/main/java/uk/gov/di/ipv/stub/core/config/uatuser/FullName.java
@@ -1,16 +1,16 @@
 package uk.gov.di.ipv.stub.core.config.uatuser;
 
+import spark.utils.StringUtils;
+
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import spark.utils.StringUtils;
 
 public record FullName(String firstName, String middleName, String surname) {
     public String fullName() {
         return Stream.of(
-                nonBlankValueOrBlank(firstName),
-                nonBlankValueOrBlank(middleName),
-                nonBlankValueOrBlank(surname))
+                        nonBlankValueOrBlank(firstName),
+                        nonBlankValueOrBlank(middleName),
+                        nonBlankValueOrBlank(surname))
                 .filter(s -> !s.isBlank())
                 .collect(Collectors.joining(" "));
     }
