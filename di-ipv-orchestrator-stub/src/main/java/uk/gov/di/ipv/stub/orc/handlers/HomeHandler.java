da1bbb4b4664e3149bd610a27c302616bede988b
mike.collingwood <mike.collingwood@digital.cabinet-office.gov.uk>
2025-04-02 12:05:25 +0100

    PYIC-8194: Update comment to specify shared-dev Selenium testing

--- di-ipv-orchestrator-stub/src/main/java/uk/gov/di/ipv/stub/orc/handlers/HomeHandler.java
+++ di-ipv-orchestrator-stub/src/main/java/uk/gov/di/ipv/stub/orc/handlers/HomeHandler.java
@@ -20,6 +20,8 @@ public class HomeHandler {
         String journeyId = UUID.randomUUID().toString();
         String userId = NON_APP_JOURNEY_USER_ID_PREFIX + UUID.randomUUID();
 
+        // The defaultEnvironment query parameter was added for shared-dev Selenium testing:
+        //
         // Dev stub deployments have to be associated to users (due to the way dev-deploy works).
         // This means we must use the main orch stub. Selenium tests with the @Build tag use the
         // "default" environment option - but since we are on the main orch stub, it defaults as
