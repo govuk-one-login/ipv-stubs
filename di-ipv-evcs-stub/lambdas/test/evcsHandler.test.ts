3427ffacecd09ff1cc95befe3601639895adbf51
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2025-05-08 09:05:13 +0100

    PYIC-8285: add extra test for duplicate VCs with the same states

--- di-ipv-evcs-stub/lambdas/test/evcsHandler.test.ts
+++ di-ipv-evcs-stub/lambdas/test/evcsHandler.test.ts
@@ -354,7 +354,16 @@ describe("evcs handlers", () => {
             {vc: "some.vc.sig", state: VcState.HISTORIC}
           ]
         }),
-        case: "duplicate vcs"
+        case: "duplicate vcs with different states"
+      },
+      {
+        request: buildPutRequest({
+          vcs: [
+            {vc: "some.vc.sig", state: VcState.CURRENT},
+            {vc: "some.vc.sig", state: VcState.CURRENT}
+          ]
+        }),
+        case: "duplicate vcs with the same states"
       },
       {
         request: buildPutRequest({ si: { jwt: undefined, vot: "P2" } }),
