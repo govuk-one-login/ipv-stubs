d3c79a046d15dc342a663d38adbbb1aab09cf868
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2025-07-01 14:13:03 +0100

    PYIC-8466: return stub journey id in f2f stub queue response

--- di-ipv-credential-issuer-stub/src/main/java/uk/gov/di/ipv/stub/cred/domain/CriResponseQueueErrorEvent.java
+++ di-ipv-credential-issuer-stub/src/main/java/uk/gov/di/ipv/stub/cred/domain/CriResponseQueueErrorEvent.java
@@ -1,5 +1,6 @@
 package uk.gov.di.ipv.stub.cred.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,17 @@ import lombok.Setter;
 public class CriResponseQueueErrorEvent {
     private String sub;
     private String state;
+
+    @JsonProperty("govuk_signin_journey_id")
+    private String journeyId = "stub-journey-id";
+
     private String error;
     private String error_description;
+
+    public CriResponseQueueErrorEvent(String sub, String state, String error, String error_description) {
+        this.sub = sub;
+        this.state = state;
+        this.error = error;
+        this.error_description = error_description;
+    }
 }
