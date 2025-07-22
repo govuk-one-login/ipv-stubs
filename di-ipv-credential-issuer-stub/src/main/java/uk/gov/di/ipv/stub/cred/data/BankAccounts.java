45c4fde0d2c8f269c0a06a52d1ea7f0dd8f33dc6
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2025-07-22 14:48:20 +0100

    PYIC-8275: Add BankAccounts.java

---
+++ di-ipv-credential-issuer-stub/src/main/java/uk/gov/di/ipv/stub/cred/data/BankAccounts.java
@@ -0,0 +1,19 @@
+package uk.gov.di.ipv.stub.cred.data;
+
+import uk.gov.di.model.BankAccountDetails;
+
+public class BankAccounts {
+
+    public final static BankAccountDetails BANK_ACCOUNT_VALID = createBankAccount("103233", "12345678");
+
+    private BankAccounts() {
+        // Replace default public constructor
+    }
+
+    private static BankAccountDetails createBankAccount(String sortCode, String accountNumber) {
+        return BankAccountDetails.builder()
+                .withSortCode(sortCode)
+                .withAccountNumber(accountNumber)
+                .build();
+    }
+}
