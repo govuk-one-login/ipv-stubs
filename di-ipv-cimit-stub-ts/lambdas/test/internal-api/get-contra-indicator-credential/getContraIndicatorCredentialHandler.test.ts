9bab7ac62769bcba32743fa03acda9b156e11aaf
Arthur Swindells <arthur.swindells@digital.cabinet-office.gov.uk>
2025-11-17 11:46:04 +0000

    PYIC-8692 clean up handler tests for readability
    And also remove trailing unused elements

--- di-ipv-cimit-stub-ts/lambdas/test/internal-api/get-contra-indicator-credential/getContraIndicatorCredentialHandler.test.ts
+++ di-ipv-cimit-stub-ts/lambdas/test/internal-api/get-contra-indicator-credential/getContraIndicatorCredentialHandler.test.ts
@@ -5,7 +5,6 @@ import {
   APIGatewayProxyStructuredResultV2,
 } from "aws-lambda";
 import {
-  ContraIndicator,
   getContraIndicatorCredentialHandler,
   GetContraIndicatorCredentialResponse,
   VcClaim,
@@ -15,7 +14,7 @@ import {
   getCimitSigningKey,
 } from "../../../src/common/configService";
 import { getCIsForUserID } from "../../../src/common/dataService";
-import { decodeJwt, jwtVerify } from "jose";
+import { decodeJwt } from "jose";
 
 jest.useFakeTimers().setSystemTime(new Date("2020-01-01"));
 
@@ -30,7 +29,6 @@ const MITIGATION_M01 = "M01";
 const TXN_1 = "1";
 const TXN_2 = "2";
 const TXN_3 = "3";
-const TXN_4 = "4";
 const ISSUANCE_DATE_1 = 1577836800; // 01/01/2020 00:00:00
 const ISSUANCE_DATE_2 = 1577836900; // 01/01/2020 00:01:40
 const ISSUANCE_DATE_3 = 1577836960; // 01/01/2020 00:02:40
@@ -49,9 +47,6 @@ const PARSED_MITIGATION_01 = {
     },
   ],
 };
-
-const CIMIT_PUBLIC_JWK =
-  '{"kty":"EC","crv":"P-256","x":"E9ZzuOoqcVU4pVB9rpmTzezjyOPRlOmPGJHKi8RSlIM","y":"KlTMZthHZUkYz5AleTQ8jff0TJiS3q2OB9L5Fw4xA04"}'; // pragma: allowlist secret
 
 const buildGetContraIndicatorCredentialRequest = (
   headers: APIGatewayProxyEventHeaders = {
@@ -85,87 +80,8 @@ beforeEach(async () => {
   );
 });
 
-// test("It returns signed JWT containing single mitigation when provided valid request", async () => {
-//   // Arrange
-//   jest.mocked(getCIsForUserID).mockResolvedValue([
-//     {
-//       userId: USER_ID,
-//       contraIndicatorCode: CI_V03,
-//       issuer: ISSUER_1,
-//       mitigations: [MITIGATION_M01],
-//       txn: TXN_1,
-//       issuanceDate: ISSUANCE_DATE_1,
-//       document: DOCUMENT_1,
-//     },
-//   ]);
-//   const validRequest = buildGetContraIndicatorCredentialRequest();
-
-//   // Act
-//   const response = (await getContraIndicatorCredentialHandler(
-//     validRequest,
-//   )) as APIGatewayProxyStructuredResultV2;
-
-//   // Assert
-//   expect(getCimitComponentId).toHaveBeenCalled();
-//   expect(getCimitSigningKey).toHaveBeenCalled();
-
-//   expect(response.statusCode).toBe(200);
-//   expect(response.body).not.toBeUndefined();
-
-//   const parsedBody = (
-//     response.body ? JSON.parse(response.body) : {}
-//   ) as GetContraIndicatorCredentialResponse;
-
-//   expect(parsedBody.vc).toBeTruthy();
-
-//   const parsedJWT = decodeJwt(parsedBody.vc);
-
-//   // assert claims on JWT returned from body
-//   expect(parsedJWT.sub).toEqual(USER_ID);
-//   expect(parsedJWT.iss).toEqual(CIMIT_COMPONENT_ID);
-
-//   // assert properties on VC claim
-//   const vcClaim = parsedJWT.vc as VcClaim;
-//   expect(vcClaim.type).toEqual([
-//     "VerifiableCredential",
-//     "SecurityCheckCredential",
-//   ]);
-
-//   const contraIndicators = vcClaim.evidence[0].contraIndicator;
-//   expect(contraIndicators.length).toEqual(1);
-
-//   const firstContraIndicator = contraIndicators[0];
-//   expect(firstContraIndicator.txn).toEqual([TXN_1]);
-
-//   const expectedCI: ContraIndicator = {
-//     code: CI_V03,
-//     document: DOCUMENT_1,
-//     issuanceDate: new Date(ISSUANCE_DATE_1 * 1000).toISOString(),
-//     issuers: [ISSUER_1],
-//     mitigation: [PARSED_MITIGATION_01],
-//     incompleteMitigation: [],
-//     txn: [TXN_1],
-//   };
-//   expect(firstContraIndicator).toEqual(expectedCI);
-
-//   const signingKey = JSON.parse(CIMIT_PUBLIC_JWK);
-//   const verifyJwtSignature = async () => {
-//     try {
-//       await jwtVerify(parsedBody.vc, signingKey, {
-//         currentDate: new Date(2021, 1, 1, 1, 0, 0, 0),
-//       });
-//       return "JWT verified";
-//     } catch (e) {
-//       return `Failed JWT verification: ${e}`;
-//     }
-//   };
-
-//   expect(await verifyJwtSignature()).toEqual("JWT verified");
-// });
-
 test.each([
-  {
-    testDescription: "return single unmitigated CI",
+  {case: "return single unmitigated CI",
     mockedDatabaseEntry: [
       {
         userId: USER_ID,
@@ -189,8 +105,7 @@ test.each([
       },
     ],
   },
-  {
-    testDescription: "return two CIs for different documents",
+  {case: "return two CIs for different documents",
     mockedDatabaseEntry: [
       {
         userId: USER_ID,
@@ -232,9 +147,7 @@ test.each([
       },
     ],
   },
-  {
-    testDescription:
-      "return two CIs for different documents, with one mitigated",
+  {case: "return two CIs for different documents, with one mitigated",
     mockedDatabaseEntry: [
       {
         userId: USER_ID,
@@ -276,9 +189,7 @@ test.each([
       },
     ],
   },
-  {
-    testDescription:
-      "return two CIs for different documents, with both mitigated",
+  {case: "return two CIs for different documents, with both mitigated",
     mockedDatabaseEntry: [
       {
         userId: USER_ID,
@@ -320,9 +231,7 @@ test.each([
       },
     ],
   },
-  {
-    testDescription:
-      "return one CI when same document submitted twice, with the same CI",
+  {case: "return one CI when same document submitted twice, with the same CI",
     mockedDatabaseEntry: [
       {
         userId: USER_ID,
@@ -355,9 +264,7 @@ test.each([
       },
     ],
   },
-  {
-    testDescription:
-      "return the unmitigated CI, when same document submitted twice with the same CIs but the one is mitigated",
+  {case: "return the unmitigated CI, when same document submitted twice with the same CIs but the one is mitigated",
     mockedDatabaseEntry: [
       {
         userId: USER_ID,
@@ -390,9 +297,7 @@ test.each([
       },
     ],
   },
-  {
-    testDescription:
-      "return one mitigated CI, when same document submitted twice with the same CIs and both are mitigated",
+  {case: "return one mitigated CI, when same document submitted twice with the same CIs and both are mitigated",
     mockedDatabaseEntry: [
       {
         userId: USER_ID,
@@ -425,9 +330,7 @@ test.each([
       },
     ],
   },
-  {
-    testDescription:
-      "consolidate duplicate non-doc CIs and keep distinct document CIs separate",
+  {case: "consolidate duplicate non-doc CIs and keep distinct document CIs separate",
     mockedDatabaseEntry: [
       {
         userId: USER_ID,
@@ -496,9 +399,7 @@ test.each([
       },
     ],
   },
-  {
-    testDescription:
-      "return two CIs when same document submitted twice, with different CIs",
+  {case: "return two CIs when same document submitted twice, with different CIs",
     mockedDatabaseEntry: [
       {
         userId: USER_ID,
@@ -540,7 +441,7 @@ test.each([
       },
     ],
   },
-])("Should $testDescription", async ({ mockedDatabaseEntry, expectedCIs }) => {
+])("Should $case", async ({ mockedDatabaseEntry, expectedCIs }) => {
   // Arrange
   jest.mocked(getCIsForUserID).mockResolvedValue(mockedDatabaseEntry);
 
@@ -594,8 +495,7 @@ test("Should return 500 for invalid signing key", async () => {
 });
 
 test.each([
-  {
-    case: "missing ip-address",
+  {case: "missing ip-address",
     headers: {
       "govuk-signin-journey-id": "someJourneyId",
     } as APIGatewayProxyEventHeaders,
@@ -603,8 +503,7 @@ test.each([
       user_id: "someId",
     } as APIGatewayProxyEventQueryStringParameters,
   },
-  {
-    case: "missing govuk-signin-journey-id",
+  {case: "missing govuk-signin-journey-id",
     headers: {
       "ip-address": "someIpAddress",
     } as APIGatewayProxyEventHeaders,
@@ -612,15 +511,13 @@ test.each([
       user_id: "someId",
     } as APIGatewayProxyEventQueryStringParameters,
   },
-  {
-    case: "missing userId",
+  {case: "missing userId",
     headers: {
       "ip-address": "someIpAddress",
     } as APIGatewayProxyEventHeaders,
     queryStringParameters: {} as APIGatewayProxyEventQueryStringParameters,
   },
-])(
-  "Should return 400 for invalid request - $case",
+])("Should return 400 for invalid request - $case",
   async ({ headers, queryStringParameters }) => {
     // Arrange
     const input = buildGetContraIndicatorCredentialRequest(
@@ -637,5 +534,3 @@ test.each([
     expect(res.statusCode).toBe(400);
   },
 );
-
-test("Should return 500 for unexpected error", async () => {});
