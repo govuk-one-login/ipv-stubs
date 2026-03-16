f120a28343ad9368b1709ade9177f1c86ec36631
Dominik Tworek <dominik.tworek@digital.cabinet-office.gov.uk>
2026-03-16 15:39:16 +0000

    PYIC-8991: Bump eslint to 10.0.3. - DCMAW-ASYNC

---
+++ di-ipv-dcmaw-async-stub/lambdas/eslint.config.cjs
@@ -0,0 +1,39 @@
+const globals = require("globals");
+const parser = require("@typescript-eslint/parser");
+const tseslint = require("typescript-eslint");
+const eslint = require("@eslint/js");
+
+module.exports = [
+  {
+    // This ignore should be removed once migrated to ES module
+    ignores: ["eslint.config.cjs"],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parser: parser,
+      ecmaVersion: 2020,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.jest,
+        expect: true,
+      },
+    },
+    rules: {
+      "no-console": "off",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-var-requires": "off",
+      "padding-line-between-statements": [
+        "error",
+        {
+          blankLine: "any",
+          prev: "*",
+          next: "*",
+        },
+      ],
+    },
+  },
+];
