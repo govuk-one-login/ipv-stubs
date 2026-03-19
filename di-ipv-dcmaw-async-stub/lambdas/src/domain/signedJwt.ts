5f7021916ef2bd2e4857d1b8a8b04925e5f093f2
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2026-03-19 09:54:52 +0000

    PYIC-8973: correct jose imports for version upgrade
    Update JWTPayload imports to use the public jose entry point instead of internal dist paths to resolve TS2307 errors after the version upgrade

--- di-ipv-dcmaw-async-stub/lambdas/src/domain/signedJwt.ts
+++ di-ipv-dcmaw-async-stub/lambdas/src/domain/signedJwt.ts
@@ -1,6 +1,4 @@
-import { importPKCS8, SignJWT } from "jose";
-import { JWTPayload } from "jose/dist/types/types";
-
+import { importPKCS8, SignJWT, JWTPayload } from "jose";
 export async function vcToSignedJwt(vc: JWTPayload, signingKey: string) {
   const formattedSigningKey = await importPKCS8(
     `-----BEGIN PRIVATE KEY-----\n${signingKey}\n-----END PRIVATE KEY-----`, // pragma: allowlist secret - the key is coming from config
