2f23bbbe2ae64eb86f554b696b37ecb8b8a1c6c4
Dominik Tworek <dominik.tworek@digital.cabinet-office.gov.uk>
2026-03-26 17:06:00 +0000

    PYIC-8458: Update PostIdentityRequest object for /identity POST endpoint.

--- di-ipv-evcs-stub/lambdas/src/domain/requests/identityEndpointRequests.ts
+++ di-ipv-evcs-stub/lambdas/src/domain/requests/identityEndpointRequests.ts
@@ -8,7 +8,10 @@ export interface StoredIdentityDetails {
   expired?: boolean; // For use through the management API
 }
 
-export type PostIdentityRequest = Omit<PutRequest, "vcs" | "si"> & {
+export type PostIdentityRequest = {
+  userId: string;
+  govuk_signin_journey_id?: string;
+  vcs?: VcDetails[];
   si: StoredIdentityDetails;
 };
 
