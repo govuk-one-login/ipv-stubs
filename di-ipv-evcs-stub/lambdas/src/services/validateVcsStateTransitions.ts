8a73506d5a2cf3b9fae8e9bf38a77db7d81d8854
Dominik Tworek <dominik.tworek@digital.cabinet-office.gov.uk>
2026-03-31 15:56:58 +0100

    PYIC-8458: Commit message to be created.
    As this become little bit of chaotic development, this changes should be moved to separate branch and be nicely organised.

---
+++ di-ipv-evcs-stub/lambdas/src/services/validateVcsStateTransitions.ts
@@ -0,0 +1,31 @@
+import { VcDetails } from "../domain/sharedTypes";
+import { getStateTransitions } from "../domain/enums";
+import { getSignatureFromJwt } from "../common/utils";
+
+export function validateVcsStateTransitions(
+  existingVcs: VcDetails[],
+  newVcs: VcDetails[],
+): void {
+  const newVcSignatures = newVcs.map(({ vc }) => getSignatureFromJwt(vc));
+
+  const existingMatchingVcs = existingVcs.filter(({ vc }) =>
+    newVcSignatures.includes(getSignatureFromJwt(vc)),
+  );
+
+  existingMatchingVcs.forEach((existingMatchingVc) => {
+    const currentState = existingMatchingVc.state;
+    const newState = newVcs.find(
+      (newVc) =>
+        getSignatureFromJwt(newVc.vc) ===
+        getSignatureFromJwt(existingMatchingVc.vc),
+    )?.state;
+    if (!newState) throw Error("Not Allowed");
+
+    const allowedTransitions = getStateTransitions(newState);
+    if (!allowedTransitions.includes(currentState)) {
+      throw new Error(
+        `State VC transition from: ${currentState} to ${newState} is not allowed`,
+      );
+    }
+  });
+}
